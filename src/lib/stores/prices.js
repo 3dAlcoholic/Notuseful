2739cc4f12b0b5816060a0898794d9b7bea84ddf
3D Alcoholic <readyteddyrepair@gmail.com>
2024-03-13 10:19:49 -0500

    Delete src directory

--- src/lib/stores/prices.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-import * as priceHelper from '$helpers/prices';
-
-export const WAVAX_USD = writable(0);
-export const FARM_TOKEN = writable(0);
-
-export const updatePrices = async() => {
-    
-}
